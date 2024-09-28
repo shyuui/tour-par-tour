44ef03d4423239279a628da5260ccfa36782d7b9
shyuu <129930689+shyuui@users.noreply.github.com>
2024-09-28 22:42:29 +0200

    main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def menu():
 def continuer(sauv1,sauv2,sauv3):
     system(effacer)
     fichier_sauv = None
-    while fichier_sauv != sauv1 or fichier_sauv != sauv2 or fichier_sauv != sauv3 or fichier_sauv == None:
+    while fichier_sauv != sauv1 and fichier_sauv != sauv2 and fichier_sauv != sauv3 or fichier_sauv == None:
         fichier_sauv = input(f'''
                              
     [continuer]
@@ -67,15 +67,29 @@ def continuer(sauv1,sauv2,sauv3):
                          
     [''')
         if fichier_sauv == 'retour':
-            __main__()
-        if fichier_sauv != sauv1 or fichier_sauv != sauv2 or fichier_sauv != sauv3:
+            return __main__()
+        if fichier_sauv != sauv1 and fichier_sauv != sauv2 and fichier_sauv != sauv3:
             system(effacer)
             print("cette sauvegarde n'existe pas veuiller saissir une option qui existe ou de cree une nouvelle sauvegarde  ")
-            print(fichier_sauv)
-            print(sauv1 , sauv2 , sauv3)
     return fichier_sauv
 
 
+def nouvelle_p():
+    system(effacer)
+    cree_p = input('''
+                   voulais vous cree une nouvelle sauvgarde
+                
+                            [oui]         [non]
+                   
+                   [''')
+    if cree_p == 'non':
+        return __main__()
+    if cree_p == 'oui':
+        with open('files/sauv.txt', 'r')as f:
+            contenue = f.read().split('\n')
+            print(contenue)
+            
+                  
 
 #------------------------------------------#main#------------------------------------------#
 
@@ -87,6 +101,8 @@ def  __main__():
     quoi_faire = menu()
     if quoi_faire == 'continuer':
         fichier_run = continuer(sauv1,sauv2,sauv3)
+    if quoi_faire == 'nouvelle partie':
+        nom_partie = nouvelle_p()
         
             
 
